56c105b19f561c9c7ffad3b07fc29f3226bdd7a6
von-loh <thomas.von-loh@partner.bmwgroup.com>
2025-07-02 08:27:01 +0200

    initial project

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+    },
+  },
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageReporters: ['html'],
+};
