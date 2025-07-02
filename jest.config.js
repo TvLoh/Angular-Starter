832b4be8a51eb204b34ac09ed5a599a60a51dd88
von-loh <thomas.von-loh@partner.bmwgroup.com>
2025-07-02 11:51:01 +0200

    Configure Jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+    },
+  },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+};
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -4,20 +4,20 @@ import { App } from './app';
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App], // Standalone-Komponente direkt in `imports` einbinden
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).toBeTruthy(); // Erwartet, dass die App-Komponente erstellt wird
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
+    fixture.detectChanges(); // Änderungen anwenden
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularBaseProject');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularBaseProject'); // Erwartet, dass der Titel korrekt gerendert wird
   });
 });
